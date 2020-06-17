eb19a34d699bf6f4bd401ae10eb40db6452b1bfe
dhanu <andreasdhanu@gmail.com>
2020-06-17 23:52:44 +0700

    implement get

--- pkg/service/v1/somewhat.go
+++ pkg/service/v1/somewhat.go
@@ -2,8 +2,14 @@ package v1
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
 
 	v1 "github.com/dhanusaputra/somewhat-server/pkg/api/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 const (
@@ -20,9 +26,27 @@ func NewServer() *Server {
 
 // GetSomething ...
 func (s *Server) GetSomething(ctx context.Context, req *v1.GetSomethingRequest) (*v1.GetSomethingResponse, error) {
+	// check if the API version requested by client is supported by server
+	if err := s.checkAPI(req.Api); err != nil {
+		return nil, err
+	}
+	data, err := s.getData("tmp/db.json")
+	if err != nil {
+		return nil, status.Error(codes.Unknown, fmt.Sprintf("failed to open file, err: %v", err))
+	}
+	if _, ok := data[req.Id]; !ok {
+		return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("failed to find %v", req.Id))
+	}
+	jsonData, err := json.Marshal(data[req.Id])
+	if err != nil {
+		return nil, status.Error(codes.Unknown, fmt.Sprintf("json has invalid format, err: %v", err))
+	}
 	return &v1.GetSomethingResponse{
-		Api:       apiVersion,
-		Something: &v1.Something{},
+		Api: apiVersion,
+		Something: &v1.Something{
+			Id:          req.Id,
+			Description: string(jsonData),
+		},
 	}, nil
 }
 
@@ -57,4 +81,31 @@ func (s *Server) ListSomething(ctx context.Context, req *v1.ListSomethingRequest
 		Api:        apiVersion,
 		Somethings: list,
 	}, nil
+}
+
+// checkAPI checks if the API version requested by client is supported by server
+func (s *Server) checkAPI(api string) error {
+	// API version is "" means use current version of the service
+	if len(api) > 0 {
+		if apiVersion != api {
+			return status.Errorf(codes.Unimplemented,
+				"unsupported API version: service implements API version '%s', but asked for '%s'", apiVersion, api)
+		}
+	}
+	return nil
+}
+
+func (s *Server) getData(path string) (map[string]interface{}, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	jsonValue, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	var res map[string]interface{}
+	json.Unmarshal(jsonValue, &res)
+	return res, nil
 }
