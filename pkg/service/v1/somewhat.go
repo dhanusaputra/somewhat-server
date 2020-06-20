3c7a7ca49074e4c5112f4d49410817904f5ea0a2
dhanu <andreasdhanu@gmail.com>
2020-06-20 14:16:16 +0700

    impl delete

--- pkg/service/v1/somewhat.go
+++ pkg/service/v1/somewhat.go
@@ -33,7 +33,7 @@ func (s *Server) GetSomething(ctx context.Context, req *v1.GetSomethingRequest) 
 	if _, ok := s.data[req.Id]; !ok {
 		return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("failed to find %v", req.Id))
 	}
-	jsonData, err := json.Marshal(s.data[req.Id])
+	b, err := json.Marshal(s.data[req.Id])
 	if err != nil {
 		return nil, status.Error(codes.Unknown, fmt.Sprintf("json has invalid format, err: %v", err))
 	}
@@ -41,13 +41,17 @@ func (s *Server) GetSomething(ctx context.Context, req *v1.GetSomethingRequest) 
 		Api: apiVersion,
 		Something: &v1.Something{
 			Id:          req.Id,
-			Description: string(jsonData),
+			Description: string(b),
 		},
 	}, nil
 }
 
 // CreateSomething ...
 func (s *Server) CreateSomething(ctx context.Context, req *v1.CreateSomethingRequest) (*v1.CreateSomethingResponse, error) {
+	// check if the API version requested by client is supported by server
+	if err := s.checkAPI(req.Api); err != nil {
+		return nil, err
+	}
 	return &v1.CreateSomethingResponse{
 		Api: apiVersion,
 		Id:  "",
@@ -56,6 +60,10 @@ func (s *Server) CreateSomething(ctx context.Context, req *v1.CreateSomethingReq
 
 // UpdateSomething ...
 func (s *Server) UpdateSomething(ctx context.Context, req *v1.UpdateSomethingRequest) (*v1.UpdateSomethingResponse, error) {
+	// check if the API version requested by client is supported by server
+	if err := s.checkAPI(req.Api); err != nil {
+		return nil, err
+	}
 	return &v1.UpdateSomethingResponse{
 		Api:     apiVersion,
 		Updated: true,
@@ -64,6 +72,14 @@ func (s *Server) UpdateSomething(ctx context.Context, req *v1.UpdateSomethingReq
 
 // DeleteSomething ...
 func (s *Server) DeleteSomething(ctx context.Context, req *v1.DeleteSomethingRequest) (*v1.DeleteSomethingResponse, error) {
+	// check if the API version requested by client is supported by server
+	if err := s.checkAPI(req.Api); err != nil {
+		return nil, err
+	}
+	if _, ok := s.data[req.Id]; !ok {
+		return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("failed to find %v", req.Id))
+	}
+	delete(s.data, req.Id)
 	return &v1.DeleteSomethingResponse{
 		Api:     apiVersion,
 		Deleted: true,
@@ -72,15 +88,19 @@ func (s *Server) DeleteSomething(ctx context.Context, req *v1.DeleteSomethingReq
 
 // ListSomething ...
 func (s *Server) ListSomething(ctx context.Context, req *v1.ListSomethingRequest) (*v1.ListSomethingResponse, error) {
+	// check if the API version requested by client is supported by server
+	if err := s.checkAPI(req.Api); err != nil {
+		return nil, err
+	}
 	res := make([]*v1.Something, 0, len(s.data))
 	for k, v := range s.data {
-		jsonV, err := json.Marshal(v)
+		b, err := json.Marshal(v)
 		if err != nil {
 			return nil, status.Error(codes.Unknown, fmt.Sprintf("json has invalid format, err: %v", err))
 		}
 		res = append(res, &v1.Something{
 			Id:          k,
-			Description: string(jsonV),
+			Description: string(b),
 		})
 	}
 	return &v1.ListSomethingResponse{
