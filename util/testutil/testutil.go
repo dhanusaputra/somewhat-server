d9a6a46d489ad86a3bee9cc458ce125ff17ba1ed
dhanu <andreasdhanu@gmail.com>
2020-08-29 14:31:55 +0700

    rename

--- util/testutil/testutil.go
+++ util/testutil/testutil.go
@@ -2,13 +2,13 @@ package testutil
 
 import "reflect"
 
-type ptr struct {
+type ptrStruct struct {
 	ptr         interface{}
 	oriFunc     interface{}
 	oriFuncType reflect.Type
 }
 type ptrs struct {
-	ptrs []ptr
+	ptrs []ptrStruct
 }
 
 // TearDownInterface ...
@@ -18,9 +18,9 @@ type TearDownInterface interface {
 
 // NewPtrs ...
 func NewPtrs(ins []interface{}) TearDownInterface {
-	outs := make([]ptr, 0, len(ins))
+	outs := make([]ptrStruct, 0, len(ins))
 	for _, in := range ins {
-		outs = append(outs, ptr{
+		outs = append(outs, ptrStruct{
 			ptr:         in,
 			oriFunc:     reflect.ValueOf(in).Elem().Interface(),
 			oriFuncType: reflect.ValueOf(in).Elem().Type(),
@@ -31,11 +31,11 @@ func NewPtrs(ins []interface{}) TearDownInterface {
 
 // Restore ...
 func (p *ptrs) Restore() {
-	for _, pair := range p.ptrs {
-		if pair.oriFunc == nil {
-			reflect.ValueOf(pair.ptr).Elem().Set(reflect.Zero(pair.oriFuncType))
+	for _, ptrStruct := range p.ptrs {
+		if ptrStruct.oriFunc == nil {
+			reflect.ValueOf(ptrStruct.ptr).Elem().Set(reflect.Zero(ptrStruct.oriFuncType))
 		} else {
-			reflect.ValueOf(pair.ptr).Elem().Set(reflect.ValueOf(pair.oriFunc))
+			reflect.ValueOf(ptrStruct.ptr).Elem().Set(reflect.ValueOf(ptrStruct.oriFunc))
 		}
 	}
 }
